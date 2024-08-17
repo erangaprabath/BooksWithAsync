2dfe2eaaf50f31f1df9d65aa178208b0e9a5c668
erangaprabath <erangaprabath@icloud.com>
2024-08-17 11:40:34 +0530

    Initial Commit

---
+++ testAPIdata/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testAPIdata
+//
+//  Created by Eranga Prabath on 2024-08-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testAPIdata/testAPIdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAPIdataApp.swift
+//  testAPIdata
+//
+//  Created by Eranga Prabath on 2024-08-17.
+//
+
+import SwiftUI
+
+@main
+struct testAPIdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
